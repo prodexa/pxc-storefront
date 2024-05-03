75f4f2da364b91405c088c13dda8b0c3da538382
Oleg Goloubitski <oleg.goloubitski@prodexa.com>
2024-05-03 11:53:40 +0300

    update log, favicon

--- src/lib/services/prodexa/init-service.ts
+++ src/lib/services/prodexa/init-service.ts
@@ -1,4 +1,4 @@
 import { websiteName } from './../../config/website'
 export const fetchInit = async (host) => {
-	return { storeOne: { id: 'Medusa', _id: 'Medusa', websiteName } }
+	return { storeOne: { id: 'Prodexa', _id: 'Prodexa', websiteName } }
 }
