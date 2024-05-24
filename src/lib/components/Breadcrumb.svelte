1788e9a8f680deec8c8bf13e3c8658f1e6aebd83
Oleg Goloubitski <oleg.goloubitski@prodexa.com>
2024-05-24 09:41:26 +0300

    revert breadcrumb

--- src/lib/components/Breadcrumb.svelte
+++ src/lib/components/Breadcrumb.svelte
@@ -55,17 +55,17 @@ export { clazz as class }
 				{d.name}
 			</a>
 
-			<!--{#if dx < categoryPool?.length - 2}-->
-			<!--	<svg-->
-			<!--		xmlns="http://www.w3.org/2000/svg"-->
-			<!--		class="h-5 w-5 px-1 shrink-0"-->
-			<!--		fill="none"-->
-			<!--		viewBox="0 0 24 24"-->
-			<!--		stroke="currentColor"-->
-			<!--		stroke-width="2">-->
-			<!--		<path stroke-linecap="round" stroke-linejoin="round" d="M9 5l7 7-7 7"></path>-->
-			<!--	</svg>-->
-			<!--{/if}-->
+			{#if dx < categoryPool?.length -1}
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					class="h-5 w-5 px-1 shrink-0"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke="currentColor"
+					stroke-width="2">
+					<path stroke-linecap="round" stroke-linejoin="round" d="M9 5l7 7-7 7"></path>
+				</svg>
+			{/if}
 		{/each}
 	{/if}
 
