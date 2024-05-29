bb00d018d5ad68c3b40919a836a59a2086ade0f1
EugeneGoroschenya <eugene.goroschenya+github@prodexa.com>
2024-05-29 09:37:59 +0300

    build(pxc): build scripts

--- devops/package.js
+++ devops/package.js
@@ -6,7 +6,7 @@ const FILE_NAMES =
 	'package.json static .svelte-kit pm2.config.cjs svelte.config.js tsconfig.json vite.config.ts .env'
 // End Config
 
-// Zip and send file to remote server
+// Zip to prepare file for deployment
 shell
 	.cd('prod')
 	.exec(`tar czf ../${packageInfo.name}-${packageInfo.version}.tar.gz ${FILE_NAMES}`)
