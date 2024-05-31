81b8e7b2ffbe75da77d650cbd7f711d86d832189
EugeneGoroschenya <eugene.goroschenya+github@prodexa.com>
2024-05-31 16:46:54 +0300

    chore(pxc): `pxc` base path

--- src/lib/components/MobileFooter.svelte
+++ src/lib/components/MobileFooter.svelte
@@ -11,6 +11,7 @@
 <script>
 import { goto } from '$app/navigation'
 import { page } from '$app/stores'
+import { base } from '$app/paths'
 
 let selectedItem = {}
 $: selectedItem.link = $page?.url.pathname
@@ -18,7 +19,7 @@ $: selectedItem.link = $page?.url.pathname
 let footerItems = [
 	{
 		name: 'Home',
-		link: '/',
+		link: `${base}/`,
 		icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
   		<path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
 		</svg>`,
@@ -29,7 +30,7 @@ let footerItems = [
 	},
 	{
 		name: 'Categories',
-		link: '/categories',
+		link: `${base}/categories`,
 		icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
 		<path stroke-linecap="round" stroke-linejoin="round" d="M3.75 12h16.5m-16.5 3.75h16.5M3.75 19.5h16.5M5.625 4.5h12.75a1.875 1.875 0 010 3.75H5.625a1.875 1.875 0 010-3.75z" />
 		</svg>`,
@@ -39,7 +40,7 @@ let footerItems = [
 	},
 	{
 		name: 'New Arrivals',
-		link: '/new-arrivals',
+		link: `${base}/new-arrivals`,
 		icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
   		<path stroke-linecap="round" stroke-linejoin="round" d="M15.362 5.214A8.252 8.252 0 0112 21 8.25 8.25 0 016.038 7.048 8.287 8.287 0 009 9.6a8.983 8.983 0 013.361-6.867 8.21 8.21 0 003 2.48z" />
   		<path stroke-linecap="round" stroke-linejoin="round" d="M12 18a3.75 3.75 0 00.495-7.467 5.99 5.99 0 00-1.925 3.546 5.974 5.974 0 01-2.133-1A3.75 3.75 0 0012 18z" />
@@ -50,7 +51,7 @@ let footerItems = [
 	},
 	{
 		name: 'Wishlist',
-		link: '/my/wishlist',
+		link: `${base}/my/wishlist`,
 		icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
   		<path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
 		</svg>`,
@@ -60,7 +61,7 @@ let footerItems = [
 	},
 	{
 		name: 'Account',
-		link: '/my',
+		link: `${base}/my`,
 		icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
   		<path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
 		</svg>`,
