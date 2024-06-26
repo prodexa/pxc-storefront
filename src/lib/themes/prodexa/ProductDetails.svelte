0e1a1884de33670d8bad6ec0560cb18c09413137
Oleg Goloubitski <oleg.goloubitski@prodexa.com>
2024-06-26 15:53:48 +0300

    show table attr separately

--- src/lib/themes/prodexa/ProductDetails.svelte
+++ src/lib/themes/prodexa/ProductDetails.svelte
@@ -43,8 +43,8 @@
   border: 1px solid red;
 }
 
-.mult-value{
-  list-style-type:none
+.flex-5{
+  flex:5 5 0%
 }
 
 </style>
@@ -1484,13 +1484,12 @@ async function updateVariant(variant) {
 					</form>
 				{/if}
 
-				<!-- Specifications -->
+				<!-- Specifications except text-table and markdown -->
 
-				{#if data.product?.specifications?.length}
+				{#if data.product?.specifications?.filter(s=> s?.type !== 'text-table' && s?.type !== 'markdown')?.length}
 					<div>
 						<div class="mb-2 flex items-center gap-2 uppercase">
 							<h5>Product Specifications</h5>
-
 							<svg
 								xmlns="http://www.w3.org/2000/svg"
 								fill="none"
@@ -1507,7 +1506,7 @@ async function updateVariant(variant) {
 						</div>
 
 						<div class="m-0 p-1 list-none flex flex-col justify-between gap-1">
-							{#each data.product?.specifications as s, index}
+							{#each data.product?.specifications?.filter(s=> s?.type !== 'text-table' && s?.type !== 'markdown') as s, index}
                 {#if (index === 0)}
                   <div class="flex flex-row gap-1 p-1">
                     <div class="flex-1">
@@ -1534,12 +1533,11 @@ async function updateVariant(variant) {
                       {@html s?.value}
                     </p>
                   </div>
-								</div>
+                </div>
 							{/each}
 						</div>
 					</div>
 				{/if}
-
 
 
 				<!-- Long Description -->
@@ -2197,7 +2195,66 @@ async function updateVariant(variant) {
 			</div>
 		</div>
 
+    <!-- Specifications only text-table and markdown -->
+    {#if data.product?.specifications?.filter(s=> s?.type === 'text-table' || s?.type === 'markdown')?.length}
+      <hr />
+      <div>
+        <div class="mb-2 flex items-center gap-2 uppercase">
+          <h5>Product Specifications</h5>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1"
+            stroke="currentColor"
+            class="shrink-0 w-5 h-5">
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25zM6.75 12h.008v.008H6.75V12zm0 3h.008v.008H6.75V15zm0 3h.008v.008H6.75V18z"
+            ></path>
+          </svg>
+        </div>
+
+        <div class="m-0 p-1 list-none flex flex-col gap-1">
+          {#each data.product?.specifications?.filter(s=> s?.type === 'text-table' || s?.type === 'markdown') as s, index}
+            {#if (index === 0)}
+              <div class="flex flex-row gap-1 p-1">
+                <div class="flex-1">
+                  <h6 class="font-bold redText">
+                    Attribute
+                  </h6>
+                </div>
+                <div class="flex-5">
+                  <h6 class="font-bold redText">
+                    Value
+                  </h6>
+                </div>
+              </div>
+              <hr class="redLine"/>
+            {/if}
+            <div class="flex flex-row gap-1 p-1">
+              <div class="flex-1">
+                <h6 class="font-medium">
+                  {s?.name || '-'}
+                </h6>
+              </div>
+              <div class="flex-5">
+                <p>
+                  {@html s?.value}
+                </p>
+              </div>
+            </div>
+          {/each}
+        </div>
+      </div>
+    {/if}
+
+    <!-- Product Variations -->
     {#if data?.moreProductDetails?.variants?.length}
+
+      <hr />
+
       <div>
         <div class="mb-2 flex items-center gap-2 uppercase">
           <h5>Product Variations</h5>
